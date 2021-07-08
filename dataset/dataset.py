4be673f3f7b6dcb501574022d52fd5faca08e0e2
Yuting Fan <69135204+ytfanfan@users.noreply.github.com>
2021-07-08 13:04:29 +0800

    Add files via upload

---
+++ dataset/dataset.py
@@ -0,0 +1,52 @@
+import json
+import torch
+from torch.utils import data
+from torchvision import transforms
+from PIL import Image
+import os
+import numpy as np
+
+def get_iCLEVR_data(root_folder,mode):
+    if mode == 'train':
+        data = json.load(open(os.path.join(root_folder,'train.json')))
+        obj = json.load(open(os.path.join(root_folder,'objects.json')))
+        img = list(data.keys())
+        label = list(data.values())
+        for i in range(len(label)):
+            for j in range(len(label[i])):
+                label[i][j] = obj[label[i][j]]
+            tmp = np.zeros(len(obj))
+            tmp[label[i]] = 1
+            label[i] = tmp
+        return np.squeeze(img), np.squeeze(label)
+    else:
+        data = json.load(open(os.path.join(root_folder,'test.json')))
+        obj = json.load(open(os.path.join(root_folder,'objects.json')))
+        label = data
+        for i in range(len(label)):
+            for j in range(len(label[i])):
+                label[i][j] = obj[label[i][j]]
+            tmp = np.zeros(len(obj))
+            tmp[label[i]] = 1
+            label[i] = tmp
+        return None, label
+
+
+class ICLEVRLoader(data.Dataset):
+    def __init__(self, root_folder, trans=None, cond=False, mode='train'):
+        self.root_folder = root_folder
+        self.mode = mode
+        self.img_list, self.label_list = get_iCLEVR_data(root_folder,mode)
+        if self.mode == 'train':
+            print("> Found %d images..." % (len(self.img_list)))
+        
+        self.cond = cond
+        self.num_classes = 24
+        
+                
+    def __len__(self):
+        """'return the size of dataset"""
+        pass
+
+    def __getitem__(self, index):
+        pass
